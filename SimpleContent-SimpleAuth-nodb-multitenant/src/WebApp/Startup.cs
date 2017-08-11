e6576e638534c94ef0f6ebad8500b9c5fccefdb9
Joe Audette <joe.audette@gmail.com>
2017-08-11 08:20:44 -0400

    add netcore1.x solutions

---
+++ SimpleContent-SimpleAuth-nodb-multitenant/src/WebApp/Startup.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using cloudscribe.SimpleContent.Models;
+using cloudscribe.FileManager.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+            builder.AddJsonFile("app-tenants-users.json");
+            builder.AddJsonFile("app-content-project-settings.json");
+            // this file name is ignored by gitignore
+            // so you can create it and use on your local dev machine
+            // remember last config source added wins if it has the same settings
+            builder.AddJsonFile("appsettings.dev.json", optional: true, reloadOnChange: true);
+            builder.AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+
+            services.AddLocalization(options => options.ResourcesPath = "GlobalResources");
+
+            ConfigureAuthPolicy(services);
+
+            services.Configure<cloudscribe.Web.SimpleAuth.Models.SimpleAuthSettings>(Configuration.GetSection("SimpleAuthSettings"));
+            services.Configure<MultiTenancyOptions>(Configuration.GetSection("MultiTenancy"));
+
+            services.AddMultitenancy<SiteSettings, CachingSiteResolver>();
+            services.AddScoped<cloudscribe.Web.SimpleAuth.Models.IUserLookupProvider, SiteUserLookupProvider>();
+            services.AddScoped<cloudscribe.Web.SimpleAuth.Models.IAuthSettingsResolver, SiteAuthSettingsResolver>();
+            services.AddCloudscribeSimpleAuth();
+
+            services.AddScoped<cloudscribe.SimpleContent.Models.IProjectQueries, cloudscribe.SimpleContent.Storage.NoDb.ConfigProjectQueries>();
+            services.AddNoDbStorageForSimpleContent();
+
+            services.AddCloudscribeNavigation(Configuration.GetSection("NavigationOptions"));
+            services.Configure<List<ProjectSettings>>(Configuration.GetSection("ContentProjects"));
+            services.AddScoped<IProjectSettingsResolver, SiteProjectSettingsResolver>();
+            services.AddScoped<IProjectSecurityResolver, cloudscribe.SimpleContent.Security.SimpleAuth.ProjectSecurityResolver>();
+            services.AddCloudscribeCommmon(Configuration);
+            services.AddSimpleContent();
+            services.AddMetaWeblogForSimpleContent(Configuration.GetSection("MetaWeblogApiOptions"));
+            services.AddSimpleContentRssSyndiction();
+            services.AddCloudscribeFileManager(Configuration);
+
+
+            // Add MVC services to the services container.
+            services.Configure<MvcOptions>(options =>
+            {
+                // options.InputFormatters.Add(new Xm)
+                options.CacheProfiles.Add("SiteMapCacheProfile",
+                     new CacheProfile
+                     {
+                         Duration = 30
+                     });
+
+                options.CacheProfiles.Add("RssCacheProfile",
+                     new CacheProfile
+                     {
+                         Duration = 100
+                     });
+
+            });
+
+            services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+            });
+
+            services.AddMvc()
+                .AddRazorOptions(options =>
+                {
+                    options.AddCloudscribeCommonEmbeddedViews();
+                    options.AddCloudscribeNavigationBootstrap3Views();
+                    options.AddEmbeddedViewsForSimpleAuth();
+                    options.AddCloudscribeSimpleContentBootstrap3Views();
+                    options.AddCloudscribeFileManagerBootstrap3Views();
+
+                    options.ViewLocationExpanders.Add(new SiteViewLocationExpander());
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory,
+            IOptions<cloudscribe.Web.SimpleAuth.Models.SimpleAuthSettings> authSettingsAccessor
+            )
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCloudscribeCommonStaticFiles();
+
+            // custom 404 and error page - this preserves the status code (ie 404)
+            app.UseStatusCodePagesWithReExecute("/Home/Error/{0}");
+
+            app.UseMultitenancy<SiteSettings>();
+
+            app.UsePerTenant<SiteSettings>((ctx, builder) =>
+            {
+                var authCookieOptions = new CookieAuthenticationOptions();
+                authCookieOptions.AuthenticationScheme = ctx.Tenant.AuthenticationScheme;
+                authCookieOptions.LoginPath = new PathString("/login");
+                authCookieOptions.AccessDeniedPath = new PathString("/");
+                authCookieOptions.AutomaticAuthenticate = true;
+                authCookieOptions.AutomaticChallenge = true;
+                authCookieOptions.CookieName = ctx.Tenant.AuthenticationScheme;
+                builder.UseCookieAuthentication(authCookieOptions);
+
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.AddSimpleContentStaticResourceRoutes();
+                routes.AddCloudscribeFileManagerRoutes();
+                routes.AddStandardRoutesForSimpleContent();
+
+
+                routes.MapRoute(
+                    name: "def",
+                    template: "{controller}/{action}"
+                    );
+
+                //routes.MapRoute(
+                //    name: "default",
+                //    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        private void ConfigureAuthPolicy(IServiceCollection services)
+        {
+            //https://docs.asp.net/en/latest/security/authorization/policies.html
+
+            services.AddAuthorization(options =>
+            {
+                // this policy currently means any user with a blogId claim can edit
+                // would require somthing more for multi tenant blogs
+                options.AddPolicy(
+                    "BlogEditPolicy",
+                    authBuilder =>
+                    {
+                        authBuilder.RequireClaim("blogId");
+                    }
+                 );
+				 
+				 options.AddPolicy(
+                    "PageEditPolicy",
+                    authBuilder =>
+                    {
+                        authBuilder.RequireRole("Admins");
+                    });
+
+                options.AddPolicy(
+                    "FileManagerPolicy",
+                    authBuilder =>
+                    {
+                        authBuilder.RequireRole("Admins");
+                    });
+
+                options.AddPolicy(
+                    "FileManagerDeletePolicy",
+                    authBuilder =>
+                    {
+                        authBuilder.RequireRole("Admins");
+                    });
+
+                // add other policies here 
+
+            });
+
+        }
+    }
+}
---
+++ cloudscribe-idserver-ef/src/OPServer/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OPServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(ILogger<HomeController> logger)
+        {
+            log = logger;
+        }
+
+        private ILogger log;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About(int? id)
+        {
+            if(id.HasValue)
+            {
+                ViewData["Message"] = $"Your application description page. for ID {id.Value}";
+            }
+            else
+            {
+                ViewData["Message"] = "Your application description page.";
+            }
+            
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        
+    }
+}
